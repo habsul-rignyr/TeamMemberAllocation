3c309a23c78be32fefcbd0880a7104e29736fb08
josh612 <joshamy@gmail.com>
2022-12-27 12:31:37 +0000

    Components added to src

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import './App.css'
 import Header from './Header'
 
+import Footer from './Footer'
+import Employees from './Employees'
+
+
 export default function App() {
   return (
     <div>
 		<Header />
+		<Employees />
+		<Footer />
 		</div>
   )
 }
